314a67269e1c154e84961294c3795aa4e926ff7d
Panuwat Chaiwongthon <panuwat.c@conicle.com>
2023-01-10 00:04:09 +0700

    main page

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,26 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.page.html',
-  styleUrls: ['./dashboard.page.scss'],
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.page.html',
+    styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-  public menuSlideOption = {
-    slidesPerView: 4,
-    initialSlide: 0,
-    speed: 400,
-    loop: true,
-  };
-  public weatherSlideOption = {
-    slidesPerView: 6.5,
-    spaceBetween: 10,
-    initialSlide: 0,
-    speed: 400,
-    loop: true
-  };
-  public walletBalance = 15000;
-  constructor() {}
+    public menuSlideOption = {
+        slidesPerView: 4,
+        initialSlide: 0,
+        speed: 400,
+        loop: true,
+    };
 
-  ngOnInit() {}
+    public bannerSlideOption = {
+        slidesPerView: 1,
+        initialSlide: 0,
+        speed: 400,
+        loop: true,
+    };
+
+    public walletBalance = 15000;
+    constructor() {}
+
+    ngOnInit() {}
 }
